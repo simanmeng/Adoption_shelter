188c013f47686060685833cc90fca488a7a95241
Simanmeng <mengsiman1@gmail.com>
2024-03-17 20:17:28 -0400

    initial commit of part 1

---
+++ forms.py
@@ -0,0 +1,48 @@
+"""Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField(
+        "Pet name",
+        validators=[InputRequired()],
+    )
+
+    species = SelectField(
+        "Species",
+        choices=[('cat','cat'), ('dog','dog'), ('porcupine','porcupine')],
+    )
+
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+
+    age = IntegerField(
+        "Age",
+        validators=[Optional(), NumberRange(min=0, max=30)],
+    )
+
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional(), Length(min=10)],
+    )
+
+class EditPetForm(FlaskForm):
+    """for editing an existing pet."""
+
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional(), Length(min=10)],
+    )
+
+    available = BooleanField("Available")
